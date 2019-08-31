bbba481ad8f555eae4caadc563bb13ce67933df5
haider <haider.shah@hotmail.com>
2019-08-31 07:54:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,8 +47,8 @@ end
 def reduce_to_all_true(source_array)
    i=0
   while i< source_array.length
-  if source_array[i]=true
-    if i=source_array.length-1
+  if source_array[i]==true
+    if i==source_array.length-1
       return true
     end
     i=i+1
