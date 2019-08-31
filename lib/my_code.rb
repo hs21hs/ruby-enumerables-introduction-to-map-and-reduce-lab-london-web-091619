9956111670ae0ca8e8d89dcdd403e46a9f285403
haider <haider.shah@hotmail.com>
2019-08-31 07:33:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -32,10 +32,12 @@ end
 return source_array
 end
 
-def reduce_to_total
+def reduce_to_total(source_array)
    i=0
+   total=0;
   while i< source_array.length
-    source_array[i]=source_array[i]+source_array[i-1]
+    s=source_array[i]+source_array[i-1]
+    total=total+s
   i=i+1
 end
 return source_array
