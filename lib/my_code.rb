dc226cfb4aec1ee6c9be65d2338b15d0697aea3c
haider <haider.shah@hotmail.com>
2019-08-31 07:25:09 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -23,4 +23,11 @@ end
 return source_array
 end
 
-map_to_square
+def map_to_square(source_array)
+  i=0
+  while i< source_array.length
+    source_array[i]=source_array[i]**2
+  i=i+1
+end
+return source_array
+end
