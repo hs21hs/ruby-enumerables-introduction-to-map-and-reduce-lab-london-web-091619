8e3a9636e93e644519f6546c411e722f5fbf4431
haider <haider.shah@hotmail.com>
2019-08-31 07:52:10 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -43,3 +43,17 @@ def reduce_to_total(source_array,st=0)
 end
 return total+st
 end
+
+def reduce_to_all_true(source_array)
+   i=0
+  while i< source_array.length
+  if source_array[i]=true
+    if i=source_array.length-1
+      return true
+    end
+    i=i+1
+  else
+    return false 
+  end
+end
+end
